e2f8209f5fb862216ba8ae12f25f32f48e4116fd
bobamorgan <bobamorgan@yandex.ru>
2015-10-17 06:49:38 +0300

    1.0.1

---
+++ ac.php
@@ -0,0 +1,29 @@
+<?
+/*
+	ФУНКЦИИ УПРАВЛЕНИЯ ДОСТУПОМ К КОНТЕНТУ
+*/
+class AccessControl {
+	/*
+		Функция AccessCheck()
+		Проверяет права доступа к контенту сайта
+	*/
+	public function AccessCheck() {
+		
+		global 	$accessControl,
+				$visitorRights,
+				$adminRights,
+				$developerRights;
+		
+		$visitorRights	 = 0; // Посетитель сайта
+		$adminRights 	 = 1; // Администратор контента
+		$developerRights = 2; // Разработчик
+		
+		if( $accessControl ) {
+			if( $_SESSION['uid'] == 2251846 ) {		// я
+				return 2;
+			}
+			else return 0;
+		} else return true;
+	}
+}
+?>
